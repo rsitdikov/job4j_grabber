56f80164d87f0f338d1b1bb7e54be0d9e9207c4e
rsitdikov <rsitdikov.usp@gmail.com>
2021-09-24 13:26:17 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/SqlRuParse.java
+++ src/main/java/SqlRuParse.java
@@ -2,19 +2,34 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import utils.SqlRuDateTimeParser;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SqlRuParse {
+
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements dates = doc.select("td[style].altCol");
-        int index = 0;
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element date = dates.get(index++);
-            System.out.println(date.text());
+        int pages = 5;
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofPattern("dd-MM-yy HH:mm");
+        for (int page = 1; page <= pages; page++) {
+            System.out.printf("Page :%s%s", page, System.lineSeparator());
+            String url = String.format(
+                    "https://www.sql.ru/forum/job-offers/%s", page
+            );
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements dates = doc.select("td[style].altCol");
+            int index = 0;
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                LocalDateTime date = new SqlRuDateTimeParser()
+                        .parse(dates.get(index++).text());
+                System.out.println(date.format(formatter));
+            }
         }
     }
 }
