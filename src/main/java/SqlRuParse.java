fd334d091cef4875178434c4cafcb3179c6f6b12
rsitdikov <rsitdikov.usp@gmail.com>
2021-09-28 15:54:27 +0500

    2.4. SqlRuParse [#285213]

--- src/main/java/SqlRuParse.java
+++ src/main/java/SqlRuParse.java
@@ -2,59 +2,67 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import utils.DateTimeParser;
 import utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
+    private final DateTimeParser dateTimeParser;
 
-    public Post detail(String link) throws IOException {
-        Document doc = Jsoup.connect(link).get();
-        String title = doc.select(".messageHeader")
-                .first()
-                .text();
-        String description = doc.select(".msgBody")
-                .get(1)
-                .text();
-        String dateTime = doc.select(".msgFooter")
-                .first()
-                .text()
-                .split(" \\[")[0];
-        LocalDateTime created = new SqlRuDateTimeParser()
-                .parse(dateTime);
-        return new Post(
-                title, link, description, created
-        );
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
 
-    public static void main(String[] args) throws Exception {
-        int pages = 5;
-        DateTimeFormatter formatter = DateTimeFormatter
-                .ofPattern("dd-MM-yy HH:mm");
-        for (int page = 1; page <= pages; page++) {
-            System.out.printf("Page :%s%s", page, System.lineSeparator());
-            String url = String.format(
-                    "https://www.sql.ru/forum/job-offers/%s", page
-            );
-            Document doc = Jsoup.connect(url).get();
-            Elements row = doc.select(".postslisttopic");
-            Elements dates = doc.select("td[style].altCol");
-            int index = 0;
-            for (Element td : row) {
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                LocalDateTime date = new SqlRuDateTimeParser()
-                        .parse(dates.get(index++).text());
-                System.out.println(date.format(formatter));
+    @Override
+    public List<Post> list(String link) {
+        List<Post> rsl = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements elements = doc.select(".postslisttopic");
+            for (Element el : elements) {
+                link = el.child(0).attr("href");
+                rsl.add(detail(link));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("Post details: ");
-        System.out.println(new SqlRuParse()
-                .detail("https://www.sql.ru/forum/1325330"
-                        + "/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t")
-                .toString());
+        return rsl;
+    }
+
+    @Override
+    public Post detail(String link) {
+        Post rsl = null;
+        try {
+            Document doc = Jsoup.connect(link).get();
+            String title = doc.select(".messageHeader")
+                    .first()
+                    .text();
+            String description = doc.select(".msgBody")
+                    .get(1)
+                    .text();
+            String dateTime = doc.select(".msgFooter")
+                    .first()
+                    .text()
+                    .split(" \\[")[0];
+            LocalDateTime created = dateTimeParser
+                    .parse(dateTime);
+            rsl = new Post(title, link, description, created);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+            String url = "https://www.sql.ru/forum/job-offers";
+            Parse parser = new SqlRuParse(new SqlRuDateTimeParser());
+            List<Post> posts = parser.list(url);
+            for (Post post : posts) {
+                System.out.println(post.toString());
+            }
     }
 }
